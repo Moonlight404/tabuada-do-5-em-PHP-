38d20e2ec249ed90ab33696d25d81ba55cfb0ed6
Moonlight404 <59587363+Moonlight404@users.noreply.github.com>
2020-03-21 18:51:19 -0300

    Fix

--- index.php
+++ index.php
@@ -7,15 +7,12 @@
     <body>
     <!-- Tabuada do cinco em PHP--->
     <?php
-        $a = 5;
-        while ($a <= 5) {
             for ($i=0; $i <= 10; $i++) { 
                 echo "$a * $i = ".$a * $i;
                 echo "<br>";           
             }
             echo "<br>";
             $a++;
-        }
         ?>
     </body>
 </html>
